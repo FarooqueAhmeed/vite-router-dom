f569e508516c4bb3d8217c5eef8c9b7a047d4633
zizo <zizopixels11@gmail.com>
2024-01-28 18:54:13 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/home';
+import About from './components/about';
+
+function App() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+             <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About/>} />
+             </Routes>
+          </BrowserRouter>
+    </>
+  )
+}
+
+export default App
